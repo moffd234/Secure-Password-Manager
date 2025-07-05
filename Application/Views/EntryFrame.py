8189371317daab5bc4eec8748b41a1afd0001405
Daniel Moffett <moffd234@gmail.com>
2025-07-05 01:07:05 -0400

    Created EntryFrame elements

--- Application/Views/EntryFrame.py
+++ Application/Views/EntryFrame.py
@@ -1,7 +1,21 @@
-from tkinter import ttk
+from tkinter import ttk, PhotoImage, Canvas
+
+from Application.Utils.PlaceholderEntry import PlaceholderEntry
+
+HOME_LOGO = './Assets/Home Logo.png'
 
 
 class EntryFrame(ttk.Frame):
 
     def __init__(self):
         super().__init__()
+
+        canvas_image = PhotoImage(file=HOME_LOGO)
+        self.canvas = Canvas(bg='light blue', highlightthickness=0, width=720, height=480)
+        self.canvas.create_image(360, 240, image=canvas_image)
+
+        self.password_entry: PlaceholderEntry = PlaceholderEntry(width=50, placeholder="Enter Password")
+        self.confirm_password_entry: PlaceholderEntry = PlaceholderEntry(width=50, placeholder="Confirm Password")
+
+        self.login: ttk.Button = ttk.Button(text="Login", width=15)
+        self.create_account_button: ttk.Button = ttk.Button(text="Create Account", width=15)
