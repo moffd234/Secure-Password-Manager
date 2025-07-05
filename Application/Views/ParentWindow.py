7ae66e5a1df3f0aa08e1e60c9d3da530bbb57721
Daniel Moffett <moffd234@gmail.com>
2025-07-05 00:48:04 -0400

    Created ParentWindow.py

---
+++ Application/Views/ParentWindow.py
@@ -0,0 +1,26 @@
+import tkinter
+from tkinter import ttk
+
+
+class ParentWindow(tkinter.Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.title("Python Casino!")
+        self.geometry("800x800")
+
+        self.container: ttk.Frame = ttk.Frame(self)
+        self.container.pack(fill="both", expand=True)
+
+    def render_frame(self, new_frame) -> None:
+        """
+        Destroys previous frame and renders new frame.
+        :param new_frame: A new Frame object.
+        :return: None
+        """
+        for frame in self.container.winfo_children():
+            frame.destroy()
+
+        frame: ttk.Frame = new_frame(self.container, self)
+
+        frame.pack(fill="both", expand=True)
