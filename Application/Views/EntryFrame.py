fc26177dc474093ce3e39237a636180375f7b496
Daniel Moffett <moffd234@gmail.com>
2025-07-06 15:29:59 -0400

    Bound elements to self

--- Application/Views/EntryFrame.py
+++ Application/Views/EntryFrame.py
@@ -8,18 +8,18 @@ HOME_LOGO = '../Assets/Home Logo.png'
 
 class EntryFrame(ttk.Frame):
 
-    def __init__(self, parent):
+    def __init__(self):
         super().__init__()
 
         self.canvas_image: PhotoImage = PhotoImage(file=HOME_LOGO)
-        self.canvas: Canvas = Canvas(bg='light blue', highlightthickness=0, width=720, height=480)
+        self.canvas: Canvas = Canvas(self, bg='light blue', highlightthickness=0, width=720, height=480)
         self.canvas.create_image(360, 240, image=self.canvas_image)
 
-        self.password_entry: PlaceholderEntry = PlaceholderEntry(width=50, placeholder="Enter Password")
-        self.confirm_password_entry: PlaceholderEntry = PlaceholderEntry(width=50, placeholder="Confirm Password")
+        self.password_entry: PlaceholderEntry = PlaceholderEntry(self, width=50, placeholder="Enter Password")
+        self.confirm_password_entry: PlaceholderEntry = PlaceholderEntry(self, width=50, placeholder="Confirm Password")
 
-        self.login_button: ttk.Button = ttk.Button(text="Login", width=15)
-        self.create_account_button: ttk.Button = ttk.Button(text="Create Account", width=15)
+        self.login_button: ttk.Button = ttk.Button(self, text="Login", width=15)
+        self.create_account_button: ttk.Button = ttk.Button(self, text="Create Account", width=15)
 
         self.place_elements()
 
