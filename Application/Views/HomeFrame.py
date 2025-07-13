a2ea2697ea4a9913612937dd9b77d8264c601ae1
Daniel Moffett <moffd234@gmail.com>
2025-07-12 22:39:13 -0400

    Made elements class properties

--- Application/Views/HomeFrame.py
+++ Application/Views/HomeFrame.py
@@ -14,17 +14,17 @@ class HomeFrame(ttk.Frame):
         self.controller = controller
 
         # Canvas
-        canvas_image = PhotoImage(file=MAIN_LOGO)
-        canvas = Canvas(width=WIN_WIDTH / 2, height=WIN_HEIGHT / 2, bg='light blue', highlightthickness=0)
-        canvas.create_image(180, 120, image=canvas_image)
+        self.canvas_image = PhotoImage(file=MAIN_LOGO)
+        self.canvas = Canvas(width=WIN_WIDTH / 2, height=WIN_HEIGHT / 2, bg='light blue', highlightthickness=0)
+        self.canvas.create_image(180, 120, image=self.canvas_image)
 
         # Labels
-        site_label = ttk.Label(self, text='Website', background="light blue", font=FONT)
-        username_label = ttk.Label(self, text='Username/Email', background="light blue", foreground='black', font=FONT)
-        password_label = ttk.Label(self, text='Password', background="light blue", font=FONT)
+        self.site_label = ttk.Label(self, text='Website', background="light blue", font=FONT)
+        self.username_label = ttk.Label(self, text='Username/Email', background="light blue", foreground='black', font=FONT)
+        self.password_label = ttk.Label(self, text='Password', background="light blue", font=FONT)
 
         # Entries
-        site_entry = PlaceholderEntry(placeholder="Enter site name", width=50, bg="light green", font=FONT)
-        username_entry = PlaceholderEntry(placeholder="Enter username", width=50, bg="light green", font=FONT)
-        password_entry = PlaceholderEntry(placeholder="Enter Password", width=50, bg="light green", font=FONT)
-        site_entry.focus()
+        self.site_entry = PlaceholderEntry(placeholder="Enter site name", width=50, bg="light green", font=FONT)
+        self.username_entry = PlaceholderEntry(placeholder="Enter username", width=50, bg="light green", font=FONT)
+        self.password_entry = PlaceholderEntry(placeholder="Enter Password", width=50, bg="light green", font=FONT)
+        self.site_entry.focus()
