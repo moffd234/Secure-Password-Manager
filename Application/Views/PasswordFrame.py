3f9f3ebac1d8d09b63406f0b22788433a8e0b4d2
Daniel Moffett <moffd234@gmail.com>
2025-07-19 00:22:03 -0400

    Created PasswordFrame elements

--- Application/Views/PasswordFrame.py
+++ Application/Views/PasswordFrame.py
@@ -10,3 +10,19 @@ class PasswordFrame(ttk.Frame):
     def __init__(self, parent: ttk.Frame, controller: 'ParentWindow'):
         super().__init__(parent)
         self.controller = controller
+
+
+        self.tree: ttk.Treeview = ttk.Treeview(self, columns=["site", "username", "password"], show="headings")
+        self.tree.heading("site", text="Site")
+        self.tree.heading("username", text="Username")
+        self.tree.heading("password", text="Password")
+
+        self.tree.column("site", width=150, anchor="w")
+        self.tree.column("username", width=200, anchor="w")
+        self.tree.column("password", width=200, anchor="w")
+
+        scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.tree.yview)
+        self.tree.configure(yscrollcommand=scrollbar.set)
+
+        self.tree.pack(side="left", fill="both", expand=True)
+        scrollbar.pack(side="right", fill="y")
